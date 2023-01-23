cf785494eb0e909692393cd6361fbdf06265f45d
carbajale99 <edgar99carbajal@gmail.com>
2023-01-22 17:25:25 -0800

    Less go

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,6 @@ const filterTrackName = (track) =>{
   track = track.replace(/[^a-zA-Z0-9]/g, '');
   return track;
 };
-// (async () => {
-//   const hints = await getSongData();
-//   const songAnswer = hints[0];
-//   const unFilteredAnswer = songAnswer.data + '\nby\n' + hints[4].data;
-//   songAnswer.data = filterTrackName(songAnswer.data);
-//   console.log(songAnswer.data);
-//   hints.shift();
-//   const songImage = hints[0];
-//   hints.shift();
-  
-//   console.log(hints);
-
-// })();
-
 
 
 const App = (props) =>{
